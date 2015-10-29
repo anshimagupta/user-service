b38be257939b005da4e00967b272a3810f0d29b9
Anshima Gupta <anshimag@andrew.cmu.edu>
2015-10-29 16:30:50 -0700

    User-Service

---
+++ src/main/java/user_service/domain/UserRepository.java
@@ -0,0 +1,12 @@
+package user_service.domain;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "user", path = "user")
+public interface UserRepository extends MongoRepository<User, String> {
+
+    public User findByName(String name);
+    public User findById(String id);
+    
+}
